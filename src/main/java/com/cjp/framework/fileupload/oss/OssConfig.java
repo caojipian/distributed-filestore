4581226310c4d1876b8636234598e96fa1add53e
caojipian <caojipian@digilink-tech.com>
2019-04-23 18:14:38 +0800

    参数判断

--- src/main/java/com/cjp/framework/fileupload/oss/OssConfig.java
+++ src/main/java/com/cjp/framework/fileupload/oss/OssConfig.java
@@ -4,6 +4,7 @@ import java.util.Properties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.Assert;
 import com.cjp.framework.fileupload.FileServerConfig;
 import com.cjp.framework.fileupload.FileUploadClientGroup;
 @Configuration
@@ -14,8 +15,11 @@ public class OssConfig {
   public FileUploadClientGroup ossClient() {
     Properties properties=fileServerConfig.properties();
     String endpoint=properties.getProperty("aliyuncs.sh.endpoint");
+    Assert.isNull(endpoint,"properties aliyuncs.sh.endpoint is null");
     String accessKeyId=properties.getProperty("aliyuncs.sh.accessKeyId");
+    Assert.isNull(accessKeyId,"properties aliyuncs.sh.accessKeyId is null");
     String secretAccessKey=properties.getProperty("aliyuncs.sh.secretAccessKey");
+    Assert.isNull(secretAccessKey,"properties aliyuncs.sh.secretAccessKey is null");
     FileUploadClientGroup fileUploadClientGroup=new OssClientGroup(endpoint,OssBeanDefinitionRegistrar.getBucketNameArray(),accessKeyId, secretAccessKey);
     return fileUploadClientGroup;
   }
